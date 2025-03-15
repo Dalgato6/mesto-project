07f653502e3800e36417c0b8cd50f0dfe731e92f
Dalgat <dalgat010203@mail.ru>
2025-03-15 16:12:33 +0300

    «Лайк» карточки

--- scripts/index.js
+++ scripts/index.js
@@ -3,20 +3,24 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
 const placesItem = cardTemplate.querySelector('.places__item');
+
 const cardImg = cardTemplate.querySelector('.card__image');
 const cardButtonDel = cardTemplate.querySelector('.card__delete-button');
 const cardtitle = cardTemplate.querySelector('.card__title');
 const cardPopup = document.querySelector('.popup_type_new-card');
 const imagePopup = document.querySelector('.popup_type_image');
+
 const profilePopup = document.querySelector('.popup_type_edit');
 let nameProfile = document.querySelector('.profile__title');
 let descriptionProfile = document.querySelector('.profile__description');
 const nameProfilePopup = profilePopup.querySelector('.popup__input_type_name');
 const descriptionProfilePopup = profilePopup.querySelector('.popup__input_type_description');
-const cardButtonLike = cardTemplate.querySelector('.card__like-button');
+
+
 const btnProfOpen = document.querySelector('.profile__edit-button');
 const btnProfClose = profilePopup.querySelector('.popup__close')
 const btnCardNew = document.querySelector('.profile__add-button');
+
 const btnCardNewClose = cardPopup.querySelector('.popup__close');
 // @todo: Функция создания карточки
 btnCardNew.addEventListener("click", () => openModal(cardPopup));
@@ -25,7 +29,7 @@ btnCardNewClose.addEventListener("click", () => closeModal(cardPopup));
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (el){
-    createCard(el.name, el.link)
+    createCard(el.name, el.link, false)
         
 });
 function imgSelect(img, link, name){
@@ -35,23 +39,39 @@ function imgSelect(img, link, name){
 
 const popupImg = imagePopup.querySelector(".popup__image");
 const popupCaption = imagePopup.querySelector(".popup__caption");
-function createCard(name, link){
+function createCard(name, link, userCard){
     const item = cardTemplate.cloneNode(true);
     let title = item.querySelector('.card__title');
     let img = item.querySelector('.card__image');
     imgSelect(img, link, name);
     title.textContent = name;
-    placesList.append(item);
 
     img.addEventListener("click", () => {
         imgSelect(popupImg, link, name)
         popupCaption.textContent = name;
         openModal(imagePopup);
+
     });
+
+    const btnImgClose = imagePopup.querySelector('.popup__close');
+    btnImgClose.addEventListener("click", () => closeModal(imagePopup));
+
+    const btnCardLike = item.querySelector('.card__like-button');
+    btnCardLike.addEventListener("click", (evt) =>{
+        evt.currentTarget.classList.toggle("card__like-button_is-active");
+    });
+
+    //const btnCardDel = cardTemplate.querySelector('.card__delete-button');
+    //btnCardDel.addEventListener('click');
+
+    if (userCard) {
+        placesList.prepend(item);
+    } else {
+        placesList.append(item);
+    }
+    
 }
 
-const btnImgClose = imagePopup.querySelector('.popup__close');
-btnImgClose.addEventListener("click", () => closeModal(imagePopup));
 
 function openModal(popup) {      
     popup.classList.add('popup_is-opened');
@@ -96,3 +116,23 @@ function handleProfileFormSubmit(evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+const cardFormElement = cardPopup.querySelector('.popup__form');
+
+const cardName = cardFormElement.querySelector('.popup__input_type_card-name');
+const cardLink = cardFormElement.querySelector('.popup__input_type_url');
+
+function handleCardFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+    const cardNameValue = cardName.value;
+    const cardLinkValue = cardLink.value;
+
+    createCard(cardNameValue, cardLinkValue, true);
+    closeModal(cardPopup);
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+cardFormElement.addEventListener('submit', handleCardFormSubmit);
